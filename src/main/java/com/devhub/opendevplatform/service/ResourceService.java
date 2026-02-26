4a6d3ef6d06124d917d6bb95c0c072bee7878cdd
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 14:08:03 -0500

    fix(service) doing the logic in the ResourceService

--- src/main/java/com/devhub/opendevplatform/service/ResourceService.java
+++ src/main/java/com/devhub/opendevplatform/service/ResourceService.java
@@ -1,4 +1,15 @@
 package com.devhub.opendevplatform.service;
 
+import com.devhub.opendevplatform.model.Resource;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public interface ResourceService {
+
+    Resource addResource(Resource resource);
+
+    List<Resource> searchResources(String keyword);
+
+    List<Resource> listAll();
 }
