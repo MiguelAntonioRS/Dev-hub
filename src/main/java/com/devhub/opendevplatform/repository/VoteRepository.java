28e3164b4be492f69ecd4e6a4c54a08e4b4a4b52
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 13:57:25 -0500

    fix(repository) doing the code logic in the VoteRepository

--- src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
+++ src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
@@ -1,4 +1,14 @@
 package com.devhub.opendevplatform.repository;
 
-public interface VoteRepository {
+import com.devhub.opendevplatform.model.Resource;
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    Optional<Vote> findByUserAndResource(User user, Resource resource);
 }
