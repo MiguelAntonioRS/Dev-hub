bd13de0c37c28853189fb700e1470fb4847f1586
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 13:33:33 -0500

    feat(entities) Creating the Vote entity

---
+++ src/main/java/com/devhub/opendevplatform/model/Vote.java
@@ -0,0 +1,29 @@
+package com.devhub.opendevplatform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "votes")
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Resource resource;
+
+    private int value; // +1 o -1
+
+    // getters y setters
+}
