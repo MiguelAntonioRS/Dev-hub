f2c4854e59e5e5c71887ab1cdc5c82a357acc1d1
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 14:06:31 -0500

    fix(service) creating a UserService interface class

---
+++ src/main/java/com/devhub/opendevplatform/service/UserService.java
@@ -0,0 +1,13 @@
+package com.devhub.opendevplatform.service;
+
+import com.devhub.opendevplatform.model.User;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public interface UserService {
+
+    User registerUser(User user);
+
+    Optional<User> findByEmail(String email);
+}
