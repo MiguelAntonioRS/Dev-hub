79f81ef69afed4d06e3d3ecffa6817624c2f1460
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 13:31:39 -0500

    feat(entities) Creating the Resource entity

---
+++ src/main/java/com/devhub/opendevplatform/model/Resource.java
@@ -0,0 +1,30 @@
+package com.devhub.opendevplatform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "resources")
+public class Resource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String link;
+
+    @ManyToOne
+    private User author;
+
+    private int votes = 0;
+
+    // getters y setters
+}
