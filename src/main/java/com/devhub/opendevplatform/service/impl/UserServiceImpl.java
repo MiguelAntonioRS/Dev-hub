d51d8b0c6657ac256a129e13caf3751796586690
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 15:11:55 -0500

    feat(service) adding a new method

--- src/main/java/com/devhub/opendevplatform/service/impl/UserServiceImpl.java
+++ src/main/java/com/devhub/opendevplatform/service/impl/UserServiceImpl.java
@@ -22,4 +22,9 @@ public class UserServiceImpl implements UserService {
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
 }
