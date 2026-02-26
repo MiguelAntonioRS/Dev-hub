4a2293aea2813f528f62655fef7b6c19e7ffb281
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 13:19:17 -0500

    first commit

---
+++ src/main/java/com/devhub/opendevplatform/OpenDevPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.opendevplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDevPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDevPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhub/opendevplatform/OpenDevPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.opendevplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDevPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
