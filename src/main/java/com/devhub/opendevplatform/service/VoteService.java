c2a8f4b8fb08ed8bb3049a829766baf31d9955cf
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 14:09:12 -0500

    fix(service) doing the logic in the VoteService

--- src/main/java/com/devhub/opendevplatform/service/VoteService.java
+++ src/main/java/com/devhub/opendevplatform/service/VoteService.java
@@ -1,4 +1,12 @@
 package com.devhub.opendevplatform.service;
 
+import com.devhub.opendevplatform.model.Resource;
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.model.Vote;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface VoteService {
+
+    Vote voteResource(User user, Resource resource, int value);
 }
