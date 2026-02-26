63254001de265738b6bf8e14062d7ae6ecacf975
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 13:56:07 -0500

    fix(repository) doing the code logic in the ResourceRepository

--- src/main/java/com/devhub/opendevplatform/repository/ResourceRepository.java
+++ src/main/java/com/devhub/opendevplatform/repository/ResourceRepository.java
@@ -1,4 +1,12 @@
 package com.devhub.opendevplatform.repository;
 
-public interface ResourceRepository {
+import com.devhub.opendevplatform.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    List<Resource> findByTitleContainingIgnoreCase(String keyword);
 }
