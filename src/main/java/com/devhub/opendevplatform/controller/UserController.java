afaa73d4349671de583eaa9bdf4d391ca07cfe20
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 14:58:29 -0500

    feat(controller) Finishing the logic in the UserController

--- src/main/java/com/devhub/opendevplatform/controller/UserController.java
+++ src/main/java/com/devhub/opendevplatform/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.devhub.opendevplatform.controller;
 
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+
+        userService.registerUser(user);
+        return "redirect:/users/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
 }
