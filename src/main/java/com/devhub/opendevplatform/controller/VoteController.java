4a09cd174dbf342c3f1ef36dcc3d0627e0a63404
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-27 00:16:03 -0500

    feat(controller) changing values

---
+++ src/main/java/com/devhub/opendevplatform/controller/VoteController.java
@@ -0,0 +1,44 @@
+package com.devhub.opendevplatform.controller;
+
+import com.devhub.opendevplatform.model.Resource;
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.service.ResourceService;
+import com.devhub.opendevplatform.service.UserService;
+import com.devhub.opendevplatform.service.VoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/votes")
+public class VoteController {
+
+    @Autowired
+    private VoteService voteService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @PostMapping("/{resourceId}/upvote")
+    public String upvote(@PathVariable Long resourceId, @RequestParam Long userId) {
+        User user = userService.findById(userId).orElseThrow();
+        Resource resource = resourceService.findById(resourceId).orElseThrow();
+        voteService.voteResource(user, resource, 1);
+        return "redirect:/resources";
+    }
+
+    @PostMapping("/{resourceId}/downvote")
+    public String downvote(@PathVariable Long resourceId, @RequestParam Long userId) {
+        User user = userService.findById(userId).orElseThrow();
+        Resource resource = resourceService.findById(resourceId).orElseThrow();
+        voteService.voteResource(user, resource, -1);
+        return "redirect:/resources";
+    }
+}
+
