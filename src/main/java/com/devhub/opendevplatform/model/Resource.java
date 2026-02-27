db96435f85fddd44d45336b7a77434e296ddf0bd
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-27 12:18:51 -0500

    feat(entity) improve the Resource class

--- src/main/java/com/devhub/opendevplatform/model/Resource.java
+++ src/main/java/com/devhub/opendevplatform/model/Resource.java
@@ -23,8 +23,55 @@ public class Resource {
 
     private String link;
 
-    @ManyToOne
-    private User author;
+    private String author;
 
     private int votes = 0;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
 }
