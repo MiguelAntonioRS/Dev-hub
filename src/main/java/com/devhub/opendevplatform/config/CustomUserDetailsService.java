fd8169fc4a0f79ff8e2e0ae7753e531c2001a6a4
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-27 11:38:30 -0500

    feat(security) creating the class CustomUserDetailsService

---
+++ src/main/java/com/devhub/opendevplatform/config/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.devhub.opendevplatform.config;
+
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+        return new CustomUserDetails(user);
+    }
+}
