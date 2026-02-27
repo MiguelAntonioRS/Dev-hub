b26698ab110d9d65a21bf7302323de742916610e
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-27 11:15:38 -0500

    fix(user) Fix register form binding: align fields with User entity properties

--- src/main/java/com/devhub/opendevplatform/model/User.java
+++ src/main/java/com/devhub/opendevplatform/model/User.java
@@ -24,4 +24,44 @@ public class User {
     private String password;
 
     private int reputation = 0;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(int reputation) {
+        this.reputation = reputation;
+    }
 }
