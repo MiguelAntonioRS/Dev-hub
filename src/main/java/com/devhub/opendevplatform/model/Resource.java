07818b571b0938a2d8dad1dda1ec7f0ac4742b41
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-03-04 12:37:58 -0500

    feat(entity) fix two innecesary attributes

--- src/main/java/com/devhub/opendevplatform/model/Resource.java
+++ src/main/java/com/devhub/opendevplatform/model/Resource.java
@@ -27,10 +27,6 @@ public class Resource {
 
     private String category;
 
-    private String language;
-
-    private String framework;
-
     private int votes = 0;
 
     public Long getId() {
@@ -87,21 +83,5 @@ public class Resource {
 
     public void setCategory(String category) {
         this.category = category;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getFramework() {
-        return framework;
-    }
-
-    public void setFramework(String framework) {
-        this.framework = framework;
     }
 }
