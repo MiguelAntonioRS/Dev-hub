00b8292fb8848106938d8096ebbdbe10cfb3393f
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-03-04 12:32:10 -0500

    feat(template) Generating get and set

--- src/main/java/com/devhub/opendevplatform/model/Resource.java
+++ src/main/java/com/devhub/opendevplatform/model/Resource.java
@@ -88,4 +88,20 @@ public class Resource {
     public void setCategory(String category) {
         this.category = category;
     }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
 }
