ab6d9a6561c291ddfe83647dd481f2cb46dc9c00
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-19 16:42:23 -0400

    feat(Service) finishing the CustomUserDetailsService

--- src/main/java/com/devhub/opendevplatform/config/CustomUserDetailsService.java
+++ src/main/java/com/devhub/opendevplatform/config/CustomUserDetailsService.java
@@ -15,9 +15,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(usernameOrEmail)
+                .orElseGet(() -> userRepository.findByEmail(usernameOrEmail)
+                        .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + usernameOrEmail)));
         return new CustomUserDetails(user);
     }
 }
