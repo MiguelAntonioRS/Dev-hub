a30495f9896f88451436d53657daa3ab591c98d3
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-19 16:53:37 -0400

    feat(Entity) finishing the Tag.java class

---
+++ src/main/java/com/devhub/opendevplatform/model/Tag.java
@@ -0,0 +1,27 @@
+package com.devhub.opendevplatform.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Resource> resources;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<Resource> getResources() { return resources; }
+    public void setResources(List<Resource> resources) { this.resources = resources; }
+}
