a1c25ddacf708050dbface57288ba180c11033df
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-19 16:55:02 -0400

    feat(Repository) finishing the NotificationRepository class

---
+++ src/main/java/com/devhub/opendevplatform/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.devhub.opendevplatform.repository;
+
+import com.devhub.opendevplatform.model.Notification;
+import com.devhub.opendevplatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+    List<Notification> findByUserAndIsReadFalseOrderByCreatedAtDesc(User user);
+    long countByUserAndIsReadFalse(User user);
+}
