b56e7378f988eae963f0cd1f2b053859405a1369
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-20 11:33:02 -0400

    feat(Controller) finishing the CommentController class

---
+++ src/main/java/com/devhub/opendevplatform/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.devhub.opendevplatform.controller;
+
+import com.devhub.opendevplatform.model.Comment;
+import com.devhub.opendevplatform.model.Resource;
+import com.devhub.opendevplatform.model.User;
+import com.devhub.opendevplatform.repository.ResourceRepository;
+import com.devhub.opendevplatform.repository.UserRepository;
+import com.devhub.opendevplatform.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private ResourceRepository resourceRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/add")
+    public String addComment(@RequestParam Long resourceId,
+                              @RequestParam Long userId,
+                              @RequestParam String content) {
+        Comment comment = new Comment();
+        comment.setContent(content);
+        
+        resourceRepository.findById(resourceId).ifPresent(comment::setResource);
+        userRepository.findById(userId).ifPresent(comment::setUser);
+        
+        commentService.addComment(comment);
+        return "redirect:/resources/" + resourceId;
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteComment(@PathVariable Long id, @RequestParam Long resourceId) {
+        commentService.deleteComment(id);
+        return "redirect:/resources/" + resourceId;
+    }
+}
