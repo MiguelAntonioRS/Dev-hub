a081e4667340bb19ac0c48ebf69e84787fa72ea2
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-20 11:25:08 -0400

    feat(Repository) finishing the VoteRepository class

--- src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
+++ src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
@@ -5,6 +5,7 @@ import com.devhub.opendevplatform.model.User;
 import com.devhub.opendevplatform.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,6 @@ public interface VoteRepository extends JpaRepository<Vote, Long> {
     Optional<Vote> findByUserAndResource(User user, Resource resource);
 
     void deleteByResourceId(Long resourceId);
+
+    List<Vote> findByUser(User user);
 }
