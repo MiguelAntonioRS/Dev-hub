35f067fc418855f6f7436870b22ae508cad5b160
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-26 12:25:16 -0400

    Feat(repository) creating VoteRepository.java

--- src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
+++ src/main/java/com/devhub/opendevplatform/repository/VoteRepository.java
@@ -4,6 +4,7 @@ import com.devhub.opendevplatform.model.Resource;
 import com.devhub.opendevplatform.model.User;
 import com.devhub.opendevplatform.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +17,7 @@ public interface VoteRepository extends JpaRepository<Vote, Long> {
     void deleteByResourceId(Long resourceId);
 
     List<Vote> findByUser(User user);
+
+    @Query("SELECT COALESCE(SUM(v.value), 0) FROM Vote v WHERE v.resource.id = :resourceId")
+    int getTotalVotesByResourceId(Long resourceId);
 }
