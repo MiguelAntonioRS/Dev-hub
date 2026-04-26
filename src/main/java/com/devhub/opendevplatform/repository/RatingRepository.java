e7c8b64a413f0f5f616fd66da70ff96534f18e83
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-26 11:33:08 -0400

    Feat(repository) creating RatingRepository.java

---
+++ src/main/java/com/devhub/opendevplatform/repository/RatingRepository.java
@@ -0,0 +1,15 @@
+package com.devhub.opendevplatform.repository;
+
+import com.devhub.opendevplatform.model.Rating;
+import com.devhub.opendevplatform.model.Resource;
+import com.devhub.opendevplatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    Optional<Rating> findByUserAndResource(User user, Resource resource);
+    List<Rating> findByResource(Resource resource);
+    List<Rating> findByUser(User user);
+    double avgByResource(Resource resource);
+}
