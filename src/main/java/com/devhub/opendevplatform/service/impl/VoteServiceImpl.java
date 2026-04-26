ef65cc6be13b11826d939dadc88ed59c5737b1a8
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-26 12:25:55 -0400

    Feat(service) updating VoteServiceImpl class

--- src/main/java/com/devhub/opendevplatform/service/impl/VoteServiceImpl.java
+++ src/main/java/com/devhub/opendevplatform/service/impl/VoteServiceImpl.java
@@ -3,6 +3,7 @@ package com.devhub.opendevplatform.service.impl;
 import com.devhub.opendevplatform.model.Resource;
 import com.devhub.opendevplatform.model.User;
 import com.devhub.opendevplatform.model.Vote;
+import com.devhub.opendevplatform.repository.ResourceRepository;
 import com.devhub.opendevplatform.repository.VoteRepository;
 import com.devhub.opendevplatform.service.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,21 +16,30 @@ public class VoteServiceImpl implements VoteService {
     @Autowired
     private VoteRepository voteRepository;
 
+    @Autowired
+    private ResourceRepository resourceRepository;
+
     @Override
     public Vote voteResource(User user, Resource resource, int value) {
 
         Optional<Vote> existingVote = voteRepository.findByUserAndResource(user, resource);
 
+        Vote vote;
         if (existingVote.isPresent()) {
-            Vote vote = existingVote.get();
+            vote = existingVote.get();
             vote.setValue(value);
-            return voteRepository.save(vote);
         } else {
-            Vote vote = new Vote();
+            vote = new Vote();
             vote.setUser(user);
             vote.setResource(resource);
             vote.setValue(value);
-            return voteRepository.save(vote);
         }
+        vote = voteRepository.save(vote);
+
+        int totalVotes = voteRepository.getTotalVotesByResourceId(resource.getId());
+        resource.setVotes(totalVotes);
+        resourceRepository.save(resource);
+
+        return vote;
     }
 }
